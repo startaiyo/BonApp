7511bd8f2b84ad7a595eb5ec95a3822587b8535e
shotaro.doi <shotaro.doi@dena.com>
2024-10-18 10:11:19 +0900

    Initial Commit

---
+++ BonApp/BonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonAppApp.swift
+//  BonApp
+//
+//  Created by Shotaro Doi on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct BonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BonAppTests/BonAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BonAppTests.swift
+//  BonAppTests
+//
+//  Created by Shotaro Doi on 2024/10/18.
+//
+
+import Testing
+@testable import BonApp
+
+struct BonAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
